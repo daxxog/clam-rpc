cf5a11ddf7a844708ea8d2d9771e2008007f0b4d
David Volm <david@volminator.com>
2015-07-22 00:59:15 +0000

    wip

--- clam-rpc.js
+++ clam-rpc.js
@@ -20,7 +20,64 @@
         root.ClamRpc = factory();
   }
 }(this, function() {
-    var ClamRpc;
+    var clamcoin = require('clamcoin'),
+        readline = require('readline'),
+        yargs = require('yargs'),
+        ClamRpc, argv;
+        
+    argv = 
+        yargs
+        .usage('Usage: clam-rpc -h [host] -u [username] -a [password] -p [port] -t [timeout]')
+        
+        .alias('h', 'host')
+        .describe('h', 'RPC host to connect to.')
+        .demand('h')
+        .nargs('h', 1)
+        
+        .alias('p', 'port')
+        .describe('p', 'The port to connect to.')
+        .default('p', '30174')
+        .nargs('p', 1)
+        
+        .alias('u', 'user')
+        .alias('u', 'username')
+        .describe('u', 'RPC username.')
+        .default('u', 'clamrpc')
+        .nargs('u', 1)
+        
+        .alias('a', 'auth')
+        .alias('a', 'pass')
+        .alias('a', 'password')
+        .describe('a', 'RPC password.')
+        .demand('a')
+        .nargs('a', 1)
+        
+        .alias('t', 'timeout')
+        .describe('t', 'Connection timeout in milliseconds.')
+        .default('t', '30000')
+        .nargs('t', 1)
+        
+        .help('help')
+        .argv;
+    
+    ClamRpc = function() {
+        this.client = new clamcoin.Client({
+            host: argv.host,
+            port: argv.port,
+            user: argv.user,
+            pass: argv.pass,
+            timeout: argv.timeout
+        });
+        
+        this.client.cmd('help', function(err, help) {
+            console.log(err, help);
+        });
+        
+        /*this.rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });*/
+    };
     
     return ClamRpc;
 }));
--- cli.js
+++ cli.js
@@ -5,6 +5,6 @@
  * http://www.apache.org/licenses/LICENSE-2.0.html  
  */
 
-var ClamRpc = require('./clam-rpc.min.js');
+var ClamRpc = require('./clam-rpc.js');
 
 new ClamRpc();
